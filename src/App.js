3b892f953979a1f366de9a3eff3705ed87f5e501
muyioden <modenike@gmail.com>
2019-01-23 13:53:09 -0600

    Changes added to the feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Here's another test!
+            I was changed and committed to the Feature branch!
           </p>
           <a
             className="App-link"
